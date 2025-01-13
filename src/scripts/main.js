4449f56103eadd417e9bcaa1033c1b82687f41a5
Mateusz Cieplak <m.cieplak97@gmail.com>
2025-01-13 20:37:49 +0100

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 'use strict';
 
 const food = {
@@ -18,10 +20,30 @@ const food = {
   },
 };
 
+const div = document.createElement('div');
+
+div.setAttribute('id', 'tree');
+document.body.appendChild(div);
+
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    if (typeof data[key] === 'object' && Object.keys(data[key]).length > 0) {
+      createTree(li, data[key]);
+    }
+
+    ul.appendChild(li);
+  }
+
+  element.appendChild(ul);
 }
 
 createTree(tree, food);
